a41760a1a5214f4de465998c25d0be40b7948f3e
Kristoff <chrystalio.k@outlook.com>
2022-10-22 11:47:45 +0700

    Refactor logic js

--- script.js
+++ script.js
@@ -1,54 +1,49 @@
-let kodePeserta = [];
-for (let i = 1; i <= 180; i++) {
-    let kode = 'Z' + i.toString().padStart(3, '0');
-    kodePeserta.push(kode);
-}
+$(document).ready(() => {
+    // Buat Perserta
+    let kodePeserta = [];
 
-let tableHadir = document.querySelector('#tb-hadir');
-let no = 1;
-for (let i = 0; i < kodePeserta.length; i++) {
-    let row = tableHadir.insertRow();
-    let cellNo = row.insertCell();
-    let cellKode = row.insertCell();
-    let cellToggle = row.insertCell();
-    cellNo.innerHTML = no;
-    cellKode.innerHTML = kodePeserta[i];
-    cellToggle.innerHTML = `<input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked">`
-    no++;
-}
+    const hadirPerserta = (checkHadir) => {
+        localStorage.setItem("kodePesertaHadir", JSON.stringify(checkHadir));
+    };
 
-// Add event listener for toggle switch, if checked, add value to array
-let toggleSwitch = document.querySelectorAll('.form-check-input');
-for (let i = 0; i < toggleSwitch.length; i++) {
-    toggleSwitch[i].addEventListener('change', function() {
-        if (this.checked) {
-            // Insert value to local storage
-            let kode = this.parentElement.parentElement.children[1].innerHTML;
-            let kodePesertaHadir = JSON.parse(localStorage.getItem('kodePesertaHadir')) || [];
-            kodePesertaHadir.push(kode);
-            localStorage.setItem('kodePesertaHadir', JSON.stringify(kodePesertaHadir));
+    for (let i = 1; i <= 180; i++) {
+        let kode = "Z" + i.toString().padStart(3, "0");
+        kodePeserta.push([
+            i,
+            kode,
+            `<input class="form-check-input" type="checkbox" value="${kode}" />`,
+        ]);
+    }
+
+    // masukan perserta ke table
+    $("#perserta").DataTable({
+        data: kodePeserta,
+        columns: [{ title: "No" }, { title: "Nama" }, { title: "Kehadiran" }],
+        responsive: true,
+    });
+
+    // input data kehadiran
+    $("#perserta").on("click", "input[type='checkbox']", (e) => {
+        let checked = e.target.checked;
+        if (checked) {
+            let checkHadir =
+                JSON.parse(localStorage.getItem("kodePesertaHadir")) || [];
+            checkHadir.push(e.target.value);
+            hadirPerserta(checkHadir);
         } else {
-            // Remove data from local storagE
-            let kode = this.parentElement.parentElement.children[1].innerHTML;
-            let kodePesertaHadir = JSON.parse(localStorage.getItem('kodePesertaHadir')) || [];
-            let index = kodePesertaHadir.indexOf(kode);
-            kodePesertaHadir.splice(index, 1);
-            localStorage.setItem('kodePesertaHadir', JSON.stringify(kodePesertaHadir));
+            let checkHadir =
+                JSON.parse(localStorage.getItem("kodePesertaHadir")) || [];
+            let newHadir = checkHadir.filter((id) => id != e.target.value);
+            hadirPerserta(newHadir);
         }
     });
-}
 
-// Check the local storage, if has value then check the toggle switch
-let kodePesertaHadir = JSON.parse(localStorage.getItem('kodePesertaHadir')) || [];
-for (let i = 0; i < kodePesertaHadir.length; i++) {
-    for (let j = 0; j < kodePeserta.length; j++) {
-        if (kodePesertaHadir[i] === kodePeserta[j]) {
-            toggleSwitch[j].checked = true;
+    // check kehadiran jika web ke referesh
+    let checkHadir = JSON.parse(localStorage.getItem("kodePesertaHadir")) || [];
+    Array.from(document.querySelectorAll("input[type=checkbox]")).map(
+        (checkbox) => {
+            if (checkHadir.includes(checkbox.value))
+                $(checkbox).prop("checked", true);
         }
-    }
-}
-
-function submit() {
-    let kodePesertaHadir = JSON.parse(localStorage.getItem('kodePesertaHadir')) || [];
-    alert(kodePesertaHadir);
-}
+    );
+});
